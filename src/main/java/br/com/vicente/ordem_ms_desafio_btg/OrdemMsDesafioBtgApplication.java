ae3f671c1d346784d7fc53fe78f025a18e1de5c8
Vicente Leonardo De Souza Lamberti <vicente.lamberti@hotmail.com>
2024-06-21 08:19:56 -0300

    Criação projeto

---
+++ src/main/java/br/com/vicente/ordem_ms_desafio_btg/OrdemMsDesafioBtgApplication.java
@@ -0,0 +1,13 @@
+package br.com.vicente.ordem_ms_desafio_btg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdemMsDesafioBtgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdemMsDesafioBtgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/vicente/ordem_ms_desafio_btg/OrdemMsDesafioBtgApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vicente.ordem_ms_desafio_btg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdemMsDesafioBtgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
